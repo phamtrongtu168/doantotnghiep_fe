17415f9c54eaf74d63a9916e8884fa2d6c849be2
Phạm Trọng Tứ <trongtu@192.168.20.101>
2024-12-07 15:13:08 +0700

    Add UI Services

--- src/pages/Room/RoomPage.jsx
+++ src/pages/Room/RoomPage.jsx
@@ -4,49 +4,52 @@ import Pagination from "../../ui/Pagination";
 import RoomCard from "../../ui/RoomCard";
 import { useLocation } from "react-router-dom";
 
-
 function RoomPage(props) {
-    const [currentPage, setCurrentPage] = useState(1);
-    const location = useLocation();
-    const dataRooms = location.state?.data;
+  const [currentPage, setCurrentPage] = useState(1);
+  const location = useLocation();
+  const dataRooms = location.state?.data;
 
-    const handlePageChange = (page) => {
-      console.log("Page:", page);
-      setCurrentPage(page);
-    };
-    console.log(dataRooms)
-    return (
-        <div className='px-16'>
-            <div className='flex justify-between bg-[#e1e1e1] p-3 rounded-md my-4'>
-                <p className='text-lg text-zinc-600'>306 Phòng</p>
-                <div className='flex gap-2'>
-                    <p className='text-lg text-zinc-600'>Sắp xếp:</p>
-                    <select className='bg-transparent border-none text-primary outline-none text-lg'>
-                        <option className='' value="">Giá (Thấp - Cao)</option>
-                        <option className='' value="">Giá (Cao - Thấp)</option>
-                    </select>
-                </div>
-            </div>
-            <div className='grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-12'>
-                {dataRooms?.map((room, index) => (
-                    <RoomCard
-                        key={index}
-                        link={room.link}
-                        imageSrc={room.imageSrc}
-                        price={room.price}
-                        name={room.name}
-                        address={room.address}
-                        details={room.details}
-                    />
-                ))}
-            </div>
-            <Pagination
-                currentPage={currentPage}
-                totalPages={5}
-                onPageChange={handlePageChange}
-            />
+  const handlePageChange = (page) => {
+    console.log("Page:", page);
+    setCurrentPage(page);
+  };
+  console.log(dataRooms);
+  return (
+    <div className="px-16">
+      <div className="flex justify-between bg-[#e1e1e1] p-3 rounded-md my-4">
+        <p className="text-lg text-zinc-600">{dataRooms.length} Phòng</p>
+        <div className="flex gap-2">
+          <p className="text-lg text-zinc-600">Sắp xếp:</p>
+          <select className="bg-transparent border-none text-primary outline-none text-lg">
+            <option className="" value="">
+              Giá (Thấp - Cao)
+            </option>
+            <option className="" value="">
+              Giá (Cao - Thấp)
+            </option>
+          </select>
         </div>
-    );
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-12">
+        {dataRooms?.map((room, index) => (
+          <RoomCard
+            key={index}
+            link={room.link}
+            image={room?.images[0]?.image_url}
+            price={room.price}
+            name={room.name}
+            address={room.address}
+            details={room.details}
+          />
+        ))}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={5}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
 }
 
 export default RoomPage;
--- src/ui/RoomCard.jsx
+++ src/ui/RoomCard.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const RoomCard = ({ link, imageSrc, price, name, address, details }) => {
+const RoomCard = ({ link, image, price, name, address, details }) => {
   return (
-    <div className='bg-[#e8e8e8] rounded-lg min-w-64 w-full'>
+    <div className="bg-[#e8e8e8] rounded-lg min-w-64 w-full">
       <Link to={link}>
-        <img className='rounded-lg aspect-[13/10] w-full' src={imageSrc} alt={name} />
+        <img
+          className="rounded-lg aspect-[13/10] w-full"
+          src={image}
+          alt={name}
+        />
       </Link>
-      <figcaption className='p-2'>
-        <strong className='block'>{price}</strong>
-        <p className='my-2'>{name}</p>
-        <p className='my-2'>{address}</p>
-        <p className='my-2'>{details}</p>
-        <button className='w-full text-base cursor-pointer mt-2 p-1 rounded text-primary border-[2px] border-solid border-primary hover:bg-primary hover:text-white'>
+      <figcaption className="p-2">
+        <strong className="block">{price}</strong>
+        <p className="my-2">{name}</p>
+        <p className="my-2">{address}</p>
+        <p className="my-2">{details}</p>
+        <button className="w-full text-base cursor-pointer mt-2 p-1 rounded text-primary border-[2px] border-solid border-primary hover:bg-primary hover:text-white">
           Đặt phòng
         </button>
       </figcaption>
